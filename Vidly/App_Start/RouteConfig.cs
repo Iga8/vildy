84c8973dfed3632c6c1aba79b99c15fc2737f26d
Iga8 <justinacieslak@gmail.com>
2017-06-22 16:46:25 +0100

    mvc-structure

---
+++ Vidly/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vidly
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //orders matters use from more sprcific to default ones 
+            
+            //better way of using routs is call this method  
+            routes.MapMvcAttributeRoutes();
+            
+            
+            
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        
+        // GET: /Movies/Random
+        public ActionResult Random()
+        {
+            //create instance of movie model 
+            var movie= new Movie(){Name = "Shrek!"};
+           
+            //create List of customers 
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer1"},
+                new Customer {Name = "Customer 2"}
+            };
+            //create view model object 
+            var viewModel = new RandomMovieViewModel
+            {
+                //initialise the movie and customer properties 
+                Movie=movie,
+                Customers = customers
+            };
+                //pass data to the view 
+            return View(viewModel);//the best approch dont use the viewData or view Bag approch 
+            }
+
+        //create new action 
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);//pramator calles id 
+        }
+
+        //route atrribiutes and apply url templent 
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]//applay regular expresion 
+        public ActionResult ByRealiseDate(int year, int month)
+            //this action takes two pramators 
+        {
+            return Content(year +"/" + month);//return content with year and month 
+
+        }
+
+	}
+
+}
